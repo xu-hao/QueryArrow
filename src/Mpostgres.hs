b15065eeacd10b504ced3ea22b1ae22e81b96c4f
xu-hao <xuh@email.unc.edu>
2015-07-18 23:28:49 -0400

    updates

--- src/Mpostgres.hs
+++ src/Mpostgres.hs
@@ -1,27 +1,13 @@
-{-# LANGUAGE MonadComprehensions, DeriveGeneric #-}
 module Mpostgres where
 
 import FO
-import Parser
 import SQL.SQL
 import SQL.HDBC
-import SQL.HDBC.Sqlite3
 import SQL.HDBC.PostgreSQL
 import DBQuery
 import ICAT
 
-import Prelude hiding (lookup)
-import Data.Map.Strict (empty, fromList,lookup)
-import Text.Parsec (runParser)
-import Data.Functor.Identity
-import Control.Monad.Trans.State.Strict (evalStateT)
 import Database.HDBC
-import System.Environment
-import Data.List (intercalate, transpose)
-import Data.Aeson
-import GHC.Generics
-import Control.Applicative ((<$>))
-import qualified Data.ByteString.Lazy as B
 
 getDB :: ICATDBConnInfo -> IO (Database DBAdapterMonad MapResultRow, Query -> String)
 getDB ps = do
