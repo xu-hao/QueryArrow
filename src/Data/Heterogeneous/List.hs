039fa7853fc42b1dd0145bc4321447c94a2080f4
Hao Xu <xuh@email.unc.edu>
2016-08-28 16:40:39 -0400

    update

--- src/Data/Heterogeneous/List.hs
+++ src/Data/Heterogeneous/List.hs
@@ -3,6 +3,7 @@ module Data.Heterogeneous.List where
 
 import Data.Constraint
 import Data.Functor.Compose
+import Data.Monoid (mempty, (<>))
 {- a simplified version of hlist -}
 
 data HList (l :: [*]) where
@@ -60,9 +61,10 @@ infixr 5 .*.
 (.*.) :: e -> HList l -> HList (e ': l)
 (.*.) = HCons
 
-type family Dual (a :: *) (b :: [*]) :: [*]
-type instance Dual a '[] = '[]
-type instance Dual a (e ': l) = (e -> a) ': Dual a l
+data Op a b = Op (b -> a)
+
+applyOp :: Op a b -> b -> a
+applyOp (Op f) b = f b
 
 type family HMap (f :: * -> *) (b :: [*]) :: [*]
 type instance HMap f '[] = '[]
@@ -72,21 +74,21 @@ type family HMapConstraint (c :: * -> Constraint) (b :: [*]) :: Constraint
 type instance HMapConstraint c (e ': l) = (c e, HMapConstraint c l)
 type instance HMapConstraint c '[] = ()
 
-hApplyLV :: HList (Dual a l) -> HVariant l -> a
+hApplyLV :: HList' (Op a) l -> HVariant l -> a
 hApplyLV l v = case v of
               HLeft e ->
                   case l of
-                      HCons f _ -> f e
+                      HCons' f _ -> applyOp f e
               HRight v' ->
                   case l of
-                      HCons _ l' -> hApplyLV l' v'
+                      HCons' _ l' -> hApplyLV l' v'
 
-hApply2CULV :: forall (c :: * -> Constraint) (f :: * -> *) b (l :: [*]). (HMapConstraint c l) => (forall a . c a => a -> f a -> b) -> HList l -> HVariant (HMap f l) -> b
+hApply2CULV :: forall (c :: * -> Constraint) (f :: * -> *) b (l :: [*]). (HMapConstraint c l) => (forall a . c a => a -> f a -> b) -> HList l -> HVariant' f l -> b
 hApply2CULV t l v = case l of
                       HCons h l' ->
                           case v of
-                              HLeft e -> t h e
-                              HRight v' -> hApply2CULV @c @f t l' v'
+                              HLeft' e -> t h e
+                              HRight' v' -> hApply2CULV @c @f t l' v'
                       HNil -> error "dependent type"
 
 hApply2CULV' :: forall (c :: * -> Constraint) (f :: * -> *) (g :: * -> *) b (l :: [*]). (HMapConstraint c l) => (forall a . c a => f a -> g a -> b) -> HList' f l -> HVariant' g l -> b
@@ -110,29 +112,44 @@ hMapCULL g l = case l of
             HNil -> HNil'
             HCons e l' -> HCons' (g e) (hMapCULL @c @f g l')
 
+hMapCUL :: forall (c :: * -> Constraint) (b :: *)  (l :: [*]) . (HMapConstraint c l) => (forall a . c a => a -> b) -> HList l -> [b]
+hMapCUL g l = case l of
+            HNil -> []
+            HCons e l' -> (g e) : (hMapCUL @c @b g l')
+
 hMapACULL :: forall (c :: * -> Constraint) (f :: * -> *)  (m :: * -> *) (l :: [*]) . (HMapConstraint c l, Applicative m) => (forall a . c a => a -> m (f a)) -> HList l -> m (HList' f l)
 hMapACULL g l = case l of
             HNil -> pure HNil'
             HCons e l' -> HCons' <$> g e <*> hMapACULL @c @f g l'
 
-hMapUVV :: forall (c :: * -> Constraint) (f :: * -> *)  (l :: [*]) . (HMapConstraint c l) => (forall a . c a => a -> f a) -> HVariant l -> HVariant' f l
-hMapUVV g l = case l of
-            HLeft e -> HLeft' (g e)
-            HRight v' -> HRight' (hMapUVV @c @f g v')
+hMapACUL :: forall (c :: * -> Constraint) (b :: *)  (m :: * -> *) (l :: [*]) . (HMapConstraint c l, Applicative m) => (forall a . c a => a -> m b) -> HList l -> m [b]
+hMapACUL g l = case l of
+            HNil -> pure []
+            HCons e l' -> (:) <$> g e <*> hMapACUL @c @b g l'
 
-hFindULV :: forall (c :: * -> Constraint) (l :: [*]) . (HMapConstraint c l) => (forall a . c a => a -> Bool) -> HList l -> Maybe (HVariant l)
-hFindULV g l = case l of
+hMapACUL_ :: forall (c :: * -> Constraint) (m :: * -> *) (l :: [*]) . (HMapConstraint c l, Applicative m) => (forall a . c a => a -> m ()) -> HList l -> m ()
+hMapACUL_ g l = case l of
+            HNil -> pure mempty
+            HCons e l' -> (<>) <$> g e <*> hMapACUL_ @c g l'
+
+hMapCUVV :: forall (c :: * -> Constraint) (f :: * -> *)  (l :: [*]) . (HMapConstraint c l) => (forall a . c a => a -> f a) -> HVariant l -> HVariant' f l
+hMapCUVV g l = case l of
+            HLeft e -> HLeft' (g e)
+            HRight v' -> HRight' (hMapCUVV @c @f g v')
+
+hFindCULV :: forall (c :: * -> Constraint) (l :: [*]) . (HMapConstraint c l) => (forall a . c a => a -> Bool) -> HList l -> Maybe (HVariant l)
+hFindCULV g l = case l of
             HNil -> Nothing
             HCons e l' -> if (g e)
                 then Just (HLeft e)
-                else HRight <$> (hFindULV @c g l')
+                else HRight <$> (hFindCULV @c g l')
 
-hApplyUL :: forall (c :: * -> Constraint) (l :: [*]) (b :: *) . (HMapConstraint c l) => (forall a . c a => a -> b) -> Int -> HList l -> Maybe b
-hApplyUL g a l = case l of
+hApplyCUL :: forall (c :: * -> Constraint) (l :: [*]) (b :: *) . (HMapConstraint c l) => (forall a . c a => a -> b) -> Int -> HList l -> Maybe b
+hApplyCUL g a l = case l of
             HNil -> Nothing
             HCons e l' -> if a == 0
                 then Just (g e)
-                else hApplyUL @c g a l'
+                else hApplyCUL @c g a l'
 
 hApplyCULV :: forall (c :: * -> Constraint) (f :: * -> *) (l :: [*]) . (HMapConstraint c l) => (forall a . c a => a -> f a) -> Int -> HList l -> Maybe (HVariant' f l)
 hApplyCULV g a l = case l of
