506863841a42b5136ac95c68ec9cad07900b5e1b
Hao Xu <xuh@email.unc.edu>
2016-12-01 20:30:23 -0500

    generate etc directory

--- QueryArrow-gen/Setup.hs
+++ QueryArrow-gen/Setup.hs
@@ -5,6 +5,13 @@ import System.Directory
 import System.Process
 import System.IO
 
+replace :: Eq a => [a] -> [a] -> [a] -> [a]
+replace [] _ _ = []
+replace s@(h : t) find repl =
+    if take (length find) s == find
+        then repl ++ replace (drop (length find) s) find repl
+        else h : replace t find repl
+
 main = defaultMainWithHooks simpleUserHooks {
             preConf = \_ _ -> do
                 createDirectoryIfMissing True "gen"
@@ -14,8 +21,20 @@ main = defaultMainWithHooks simpleUserHooks {
                 withFile "gen/schema.sql" AppendMode $ \handle ->
                     hPutStrLn handle add
                 return emptyHookedBuildInfo,
-            postBuild = \_ _ _ _ ->
-                callCommand "stack exec schema_parser_main",
-            postClean = \_ _ _ _ ->
+            postBuild = \_ _ _ _ -> do
+                callCommand "stack exec schema_parser_main"
+                createDirectoryIfMissing True "etc"
+                createDirectoryIfMissing True "etc/QueryArrow"
+                createDirectoryIfMissing True "etc/QueryArrow/gen"
+                createDirectoryIfMissing True "etc/QueryArrow/gen/SQL"
+                copyFile "gen/ICATGen" "etc/QueryArrow/gen/ICATGen"
+                copyFile "gen/SQL/ICATGen" "etc/QueryArrow/gen/SQL/ICATGen"
+                copyFile "rewriting-plugin-common.rules" "etc/QueryArrow/rewriting-plugin-common.rules"
+                copyFile "rewriting-plugin.rules" "etc/QueryArrow/rewriting-plugin.rules"
+                conf <- readFile "tdb-plugin-gen.json"
+                writeFile "etc/QueryArrow/tdb-plugin-gen-abs.json" (replace conf "../QueryArrow-gen" "/etc/irods/QueryArrow")
+                ,
+            postClean = \_ _ _ _ -> do
                 removeDirectoryRecursive "gen"
+                removeDirectoryRecursive "etc"
       }
