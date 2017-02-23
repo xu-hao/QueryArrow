a27f5ee60fa2b0e34fc336a2674d074e571b2c7b
Hao Xu <xuh@email.unc.edu>
2017-02-23 01:23:35 -0500

    split plugin 2

---
+++ QueryArrow-db-include/src/QueryArrow/Include.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE MultiParamTypeClasses, DeriveGeneric, TypeSynonymInstances, FlexibleInstances #-}
+
+module QueryArrow.Include where
+
+import Data.Aeson
+import QueryArrow.Plugin
+import GHC.Generics
+import QueryArrow.DB.DB
+import QueryArrow.Config
+
+data IncludePlugin = IncludePlugin
+
+newtype IncludePluginConfig = IncludePluginConfig {
+    include :: String
+} deriving Generic
+
+instance FromJSON IncludePluginConfig
+
+instance ToJSON IncludePluginConfig
+
+instance Plugin IncludePlugin MapResultRow where
+  getDB _ getDB0 ps0 = do
+    let ps = getDBSpecificConfig ps0
+    ps3 <- getConfig (include ps)
+    getDB0 ps3
