5f6760f4e53dd753d02994418c125bb63cc2410a
Hao Xu <xuh@email.unc.edu>
2017-06-23 11:54:37 -0400

    test tools

---
+++ test-QueryArrow/src/TestDedupper.hs
@@ -0,0 +1,66 @@
+-- https://rosettacode.org/wiki/Find_duplicate_files#Haskell
+
+import Crypto.Hash        (hash, Digest)
+import Crypto.Hash.Algorithms (MD5)
+import Data.ByteString as BS  (readFile)
+import System.Environment     (getArgs, getProgName)
+import System.Directory       (doesDirectoryExist, getDirectoryContents)
+import System.FilePath.Posix  ((</>), takeFileName)
+import Control.Monad          (forM)
+import Text.Printf            (printf)
+import System.IO              (withFile, IOMode(ReadMode), hFileSize)
+
+
+type File = (Digest MD5, -- md5hash
+             FilePath)      -- filepath
+
+type FileSize = Integer
+
+getRecursiveContents :: FilePath -> FileSize -> IO [File]
+getRecursiveContents curDir maxsize = do
+  names <- getDirectoryContents curDir
+  let dirs = filter (`notElem` [".", ".."]) names
+  files <- forM dirs $ \path -> do
+             let path' = curDir </> path
+             exists <- doesDirectoryExist path'
+             if exists
+                then getRecursiveContents path' maxsize
+                else genFileHash path' maxsize
+  return $ concat files
+
+
+genFileHash :: FilePath -> FileSize -> IO [File]
+genFileHash path maxsize = do
+  size <- withFile path ReadMode hFileSize
+  if size <= maxsize
+    then BS.readFile path >>= \bs -> return [(hash bs :: Digest MD5, path)]
+    else return []
+
+findDuplicates :: FilePath -> FileSize -> IO ()
+findDuplicates dir bytes = do
+  exists <- doesDirectoryExist dir
+  if exists
+    then getRecursiveContents dir bytes >>= findSameHashes
+    else printf "Sorry, the directory \"%s\" does not exist...\n" dir
+
+findSameHashes :: [File] -> IO ()
+findSameHashes []     = return ()
+findSameHashes ((hash, fp):xs) = do
+  case lookup hash xs of
+    (Just dupFile) -> do
+      printf "===========================\n\
+                            \Found duplicate:\n\
+                            \=> %s \n\
+                            \=> %s \n\n" fp dupFile
+      writeFile ("skip\n" ++ takeFileName dupFile) fp
+      findSameHashes xs
+    (_)            -> findSameHashes xs
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [dir] -> findDuplicates dir 0
+    _ -> do
+      name <- getProgName
+      printf "Something went wrong - please use ./%s <dir> <bytes>\n" name
--- test-QueryArrow/src/TestRunner.hs
+++ test-QueryArrow/src/TestRunner.hs
@@ -8,6 +8,95 @@ import QueryArrow.Chopper.Data
 import Test.Hspec
 import Control.Monad
 
+import Text.Parsec
+import qualified Text.Parsec.Token as P
+import Control.Monad.IO.Class
+
+lexer :: P.GenTokenParser String () IO
+lexer = P.makeTokenParser P.LanguageDef {
+           P.reservedNames = ["skip"],
+           P.commentStart = "",
+           P.commentEnd = "",
+           P.commentLine = "",
+           P.nestedComments = False,
+           P.identStart = letter,
+           P.identLetter = letter <|> digit <|> oneOf "._-",
+           P.opStart = oneOf "",
+           P.opLetter = oneOf "",
+           P.reservedOpNames = [],
+           P.caseSensitive = True
+           }
+
+identifier :: ParsecT String () IO String
+identifier = P.identifier lexer
+
+integer :: ParsecT String () IO Integer
+integer = P.integer lexer
+
+
+reserved :: String -> ParsecT String () IO ()
+reserved = P.reserved lexer
+
+testp :: Bool -> String -> Maybe Handle -> ParsecT String () IO ()
+testp dryrun addr h0 = do
+  try eof <|> try (do
+      reserved "skip"
+      ref <- identifier
+      liftIO $ putStrLn ("skipped " ++ ref)
+      ) <|> try (do
+      replicateM_ 10 (char '=')
+      n <- integer
+      _ <- newline
+      act0 <- replicateM (fromInteger n) anyChar
+      _ <- newline
+      let act = read act0
+      case act of
+        SendAction qs -> do
+                h1 <- liftIO $ if dryrun
+                          then do
+                            putStrLn ("send " ++ show qs)
+                            return h0
+                          else do
+                            h <- getConnection addr h0
+                            sendMsgPack h qs
+                            case qs of
+                              QuerySet _ Quit _ -> do
+                                hClose h
+                                return Nothing
+                              _ ->
+                                return (Just h)
+                testp dryrun addr h1
+        RecvAction rs -> do
+                h1 <- liftIO $ if dryrun
+                  then do
+                    putStrLn ("recv " ++ show rs)
+                    return h0
+                  else do
+                    h <- getConnection addr h0
+                    rs2 <- receiveMsgPack h :: IO (Maybe ResultSet)
+                    show rs `shouldBe` show rs2
+                    return (Just h)
+                testp dryrun addr h1)
+
+getConnection :: String -> Maybe Handle -> IO Handle
+getConnection addr h0 = do
+        case h0 of
+           Nothing -> connect1 addr
+           Just h -> return h
+
+connect1 :: String -> IO Handle
+connect1 addr = do
+                    sock <- socket AF_UNIX Stream defaultProtocol
+                    connect sock (SockAddrUnix addr)
+                    socketToHandle sock ReadWriteMode
+
+connect2 :: Bool -> String -> IO (Maybe Handle)
+connect2 dryrun addr =
+                      if dryrun
+                         then return Nothing
+                         else Just <$> connect1 addr
+
+
 main :: IO ()
 main = do
   dryrun <- read <$> getEnv "dryrun"
@@ -18,48 +107,9 @@ main = do
   hspec $ do
     describe "all tests" $ do
       mapM_ (\filepath -> it filepath $ do
-            cnt <- readFile (inp ++ "/" ++ filepath)
-            let test = read cnt :: [Action]
+            h0 <- openFile (inp ++ "/" ++ filepath) ReadMode
+            cnt <- hGetContents h0
             h <- connect2 dryrun udsaddr
-            runTest dryrun udsaddr h test) files where
-        connect2 dryrun addr =
-          if dryrun
-             then return Nothing
-             else Just <$> connect1 addr
-        connect1 addr = do
-                 sock <- socket AF_UNIX Stream defaultProtocol
-                 connect sock (SockAddrUnix addr)
-                 socketToHandle sock ReadWriteMode
-        runTest :: Bool -> String -> Maybe Handle -> [Action] -> IO ()
-        runTest _ _ _ [] = return ()
-        runTest dryrun addr h0 (act : rest) = do
-          case act of
-            SendAction qs -> do
-                    h1 <- if dryrun
-                              then do
-                                putStrLn ("send " ++ show qs)
-                                return h0
-                              else do
-                                h <- getConnection addr h0
-                                sendMsgPack h qs
-                                case qs of
-                                  QuerySet _ Quit _ -> do
-                                    hClose h
-                                    return Nothing
-                                  _ ->
-                                    return h0
-                    runTest dryrun addr h1 rest
-            RecvAction rs -> do
-                    if dryrun
-                      then
-                        putStrLn ("recv " ++ show rs)
-                      else do
-                        h <- getConnection addr h0
-                        rs2 <- receiveMsgPack h :: IO (Maybe ResultSet)
-                        show rs `shouldBe` show rs2
-                    runTest dryrun addr h0 rest
-        getConnection :: String -> Maybe Handle -> IO Handle
-        getConnection addr h0 = do
-                case h0 of
-                   Nothing -> connect1 addr
-                   Just h -> return h
+            res <- runParserT (testp dryrun udsaddr h) () filepath cnt
+            res `shouldBe` Right ()
+            hClose h0) files where
