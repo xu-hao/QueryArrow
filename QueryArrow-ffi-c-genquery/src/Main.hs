ac3138aa6a7aa56bb6f0fd27d564735c09d710b3
Hao Xu <xuh@cs.unc.edu>
2017-07-12 16:24:44 -0400

    genquery translator cli

---
+++ QueryArrow-ffi-c-genquery/src/Main.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE FlexibleContexts, MultiParamTypeClasses, FlexibleInstances, OverloadedStrings, GADTs, ExistentialQuantification, ForeignFunctionInterface #-}
+
+import QueryArrow.FO.Utils
+
+import Prelude hiding (lookup)
+import qualified Data.Text as Text
+import Data.Map.Strict (lookup)
+import Foreign.StablePtr
+import Foreign.Ptr
+import Foreign.C.Types
+import Foreign.C.String
+import Foreign.Storable
+import Foreign.Marshal.Array
+import System.Log.Logger (errorM, infoM, debugM)
+import Data.Maybe (fromMaybe)
+import Control.Monad.Trans.Either
+import Text.Parsec (runParser)
+
+import QueryArrow.FFI.GenQuery.Parser
+import QueryArrow.FFI.Service
+import QueryArrow.FFI.Auxiliary
+import QueryArrow.FFI.GenQuery.Translate
+import QueryArrow.FFI.GenQuery.Data
+import QueryArrow.FO.Data
+import System.Environment
+
+main :: IO ()
+main = do
+  [qu, dists, accs, ticket, uz, un] <- getArgs
+  let dist = read dists
+  let acc = read accs
+  let (vars, form) = case runParser genQueryP () "" qu of
+                Left err -> error (show err)
+                Right gq -> translateGenQueryToQAL (dist /= 0) (case acc of
+                                                                    1 -> if null ticket then UserAccessControl uz un else TicketAccessControl ticket 
+                                                                    -1 -> NoAccessControl
+                                                                    0 -> AccessControlTicket) gq
+  putStrLn (show vars ++ " " ++ serialize form) 
